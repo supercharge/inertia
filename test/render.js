45865e024e08dc76962c7a5e7300076867c552bd
Marcus Pöhls <mp@seda.digital>
2022-07-26 09:13:12 +0200

    wip render tests

--- test/render.js
+++ test/render.js
@@ -1,25 +1,62 @@
 'use strict'
 
-const Koa = require('koa')
+const { expect } = require('expect')
 const Supertest = require('supertest')
 const { test } = require('@japa/runner')
-const { createApp: setupApp } = require('./helpers')
-const { HttpContext } = require('@supercharge/http')
+const { createApp } = require('./helpers')
+const { Server } = require('@supercharge/http')
 
-test.group('InertiaServiceProvider', () => {
-  test('register inertia service provider', async () => {
-    const app = await setupApp()
+test.group('Inertia render response', () => {
+  test('fails when not providing a component name', async () => {
+    const app = await createApp()
+    const server = new Server(app)
 
-    const server = new Koa().use(ctx => {
-      const { response } = HttpContext.wrap(ctx, app)
+    server.use(({ response }) => {
+      return response.inertia().render()
+    })
 
+    const response = await Supertest(server.callback())
+      .get('/')
+      .expect(500)
+
+    expect(response.body).toMatchObject({
+      statusCode: 500,
+      message: 'Missing component name when calling "response.inertia().render(<component>)"'
+    })
+  })
+
+  test('returns HTML on initial request', async () => {
+    const app = await createApp()
+    const server = new Server(app)
+
+    server.use(({ response }) => {
       return response.inertia().render('Users/List', {
         users: [{ name: 'Marcus' }]
       })
     })
 
-    await Supertest(server.callback())
+    app.config()
+      .set('http.port', 1234)
+      .set('http.host', 'localhost')
+
+    await server.start()
+
+    const response = await Supertest(server.startedServer())
       .get('/')
       .expect(200)
-  }).skip()
+
+    await server.stop()
+
+    expect(response.text).toEqual(`<!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Supercharge Inertia</title>
+    </head>
+    <body>
+      <div id="app" data-page="{&quot;component&quot;:&quot;Users/List&quot;,&quot;props&quot;:{&quot;users&quot;:[{&quot;name&quot;:&quot;Marcus&quot;}]},&quot;version&quot;:&quot;1.0.0&quot;,&quot;url&quot;:&quot;http://127.0.0.1:1234/&quot;}"></div>    </body>
+    </html>
+`)
+  })
 })
