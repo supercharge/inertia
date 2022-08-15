54cbdf0840e9ce663ce6c3656246b8bf6c68976e
Marcus Pöhls <mp@seda.digital>
2022-08-15 15:11:17 +0200

    add comments

--- src/contracts/config-contract.ts
+++ src/contracts/config-contract.ts
@@ -25,6 +25,27 @@ export interface InertiaOptions {
    * a function, you may also use an async function resolving to a string.
    */
   version?: InertiaVersion
+
+  /**
+   * This setting configures the server-side rendering when using Inertia. You
+   * can disable server-side rendering generally by using the toggle setting
+   * `enabled`. When enabled, you must provide a render function file path.
+   */
+  ssr?: {
+    /**
+     * This setting controls whether server-side rendering is enabled or not.
+     * SSR is useful when you want to render the application on the server
+     * and deliver the rendered HTML to the client.
+     */
+    enabled?: boolean
+
+    /**
+     * This setting defines the file path that exposes a render function. The
+     * render function is required for Inertia to create the rendered HTML.
+     * Your file may use a default export or a named "render" export.
+     */
+    resolveRenderFunctionFrom?: string
+  }
 }
 
 /**
