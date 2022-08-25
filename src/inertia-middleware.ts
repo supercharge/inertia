95e9ef7b2ca99b32f954daf089277a91b9e554fe
Marcus Pöhls <mp@seda.digital>
2022-08-25 10:31:33 +0200

    add share method

--- src/inertia-middleware.ts
+++ src/inertia-middleware.ts
@@ -9,7 +9,11 @@ export class HandleInertiaRequestsMiddleware {
    * @param {HttpContext} ctx
    * @param {NextHandler} next
    */
-  async handle ({ request, response }: HttpContext, next: NextHandler): Promise<any> {
+  async handle (ctx: HttpContext, next: NextHandler): Promise<any> {
+    const { request, response } = ctx
+
+    request.inertia().share(await this.share(ctx))
+
     await next()
 
     response.header('Vary', 'X-Inertia')
@@ -20,6 +24,19 @@ export class HandleInertiaRequestsMiddleware {
 
     if (response.hasStatus(302) && request.isMethod(['POST', 'PUT', 'PATCH'])) {
       response.status(303)
+    }
+  }
+
+  /**
+   * Returns the props that are shared by default.
+   *
+   * @see https://inertiajs.com/shared-data
+   *
+   * @param  {HttpContext} _ctx
+   */
+  async share (_ctx: HttpContext): Promise<Record<string, unknown>> {
+    return {
+      //
     }
   }
 }
