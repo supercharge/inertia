c32c55dfcb0f973cf802c85ec1904e80e23e9884
Marcus Pöhls <mp@seda.digital>
2023-12-03 12:31:56 +0100

    import generic import instead of relying on default export

--- src/utils.ts
+++ src/utils.ts
@@ -1,21 +1,26 @@
 'use strict'
 
-import { resolveDefaultImport } from '@supercharge/goodies'
-
-type RenderFunction = Function | { render: Function }
+interface RenderFunction {
+  default?: Function | { render?: Function }
+  render?: Function
+}
 
 /**
  * Returns the SSR "render" function from the given `path`.
  */
 export async function resolveRenderFunctionFrom (renderFunctionPath: string): Promise<Function> {
-  const renderFn = await resolveDefaultImport<{ default: RenderFunction | undefined }>(renderFunctionPath)
+  const renderFn: RenderFunction = await import(renderFunctionPath)
 
-  if (typeof renderFn === 'function') {
-    return renderFn
+  if (typeof renderFn.render === 'function') {
+    return renderFn.render
   }
 
-  if (typeof renderFn === 'object' && typeof renderFn.render === 'function') {
-    return renderFn.render
+  if (typeof renderFn.default === 'function') {
+    return renderFn.default
+  }
+
+  if (typeof renderFn.default === 'object' && typeof renderFn.default.render === 'function') {
+    return renderFn.default.render
   }
 
   throw new Error(`Inertia SSR is enabled but no "render" function is exported in "${renderFunctionPath}".`)
